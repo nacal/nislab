e6357dadf79cff003fb10da42c2a735092b2cece
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-02 23:51:31 +0900

    Develop components

---
+++ components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="card">
+    <h2 class="card__title">{{ title }}</h2>
+    <p class="card__date">{{ date }}</p>
+    <img :src="img" alt="" class="card__img" />
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  width: 320px;
+
+  &__title {
+    order: 1;
+    width: 75%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  &__date {
+    order: 2;
+    width: 25%;
+    font-size: 0.8rem;
+    line-height: 1.5rem;
+  }
+
+  &__img {
+    order: 0;
+    width: 100%;
+    height: 180px;
+    object-fit: cover;
+    border: 2px #000 solid;
+  }
+}
+</style>
---
+++ components/Cards.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cards">
+    <Card
+      v-for="post in posts"
+      :id="post.id"
+      :key="post.id"
+      :title="post.title.rendered"
+      :date="$moment(post.date).format('YYYY-MM-DD')"
+      :img="
+        post._embedded['wp:featuredmedia'][0].media_details.sizes.medium[
+          'source_url'
+        ]
+      "
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  gap: 64px 32px;
+  justify-items: center;
+  max-width: 100%;
+}
+</style>
