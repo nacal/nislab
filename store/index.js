e0ed74ae1e9c0a4010d76fc5b77954ca4ab98116
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-06 21:57:32 +0900

    Fix store/index.js

--- store/index.js
+++ store/index.js
@@ -3,7 +3,18 @@ export const state = () => ({
 })
 
 export const mutations = {
-  setData(state, res) {
+  setPosts(state, res) {
     state.posts = res
+  },
+}
+
+export const actions = {
+  async getPosts({ commit }) {
+    await this.$axios
+      .get(this.$config.apiUrl + '/posts?_embed')
+      .then((res) => {
+        commit('setPosts', res.data)
+      })
+      .catch()
   },
 }
