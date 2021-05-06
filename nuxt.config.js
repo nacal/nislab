011b8271ff73ae15df8fed99e9615d313c87a664
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-07 04:13:24 +0900

    Add swiper.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: [{ src: '~/assets/_sass/common.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/swiper', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
