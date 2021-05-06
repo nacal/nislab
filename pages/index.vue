f09e5953402b0fb50e140b8cce9a8855439aea17
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-06 21:58:06 +0900

    Change dispatch

--- pages/index.vue
+++ pages/index.vue
@@ -5,23 +5,5 @@
 </template>
 
 <script>
-import axios from 'axios'
-
-export default {
-  async asyncData({ $config, store }) {
-    return await axios
-      .get($config.apiUrl + '/posts?_embed')
-      .then((res) => {
-        store.commit('setData', res.data)
-      })
-      .catch(() => {
-        return {}
-      })
-  },
-  computed: {
-    posts() {
-      return this.$store.state.posts
-    },
-  },
-}
+export default {}
 </script>
