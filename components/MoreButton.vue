0c790c54859bc6d889abd62d2e730f9cca1fe817
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-07 19:54:54 +0900

    Develop components

---
+++ components/MoreButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <nuxt-link :to="linkTo" class="button" role="button"
+    ><span>more</span></nuxt-link
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    linkTo: {
+      type: String,
+      required: true,
+      default: './',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  position: relative;
+  display: inline-block;
+  width: 240px;
+  padding: 1rem 4rem;
+  overflow: hidden;
+  color: #fff;
+  text-align: center;
+  background: #000;
+  border-radius: 30px;
+  transition: all 0.3s;
+
+  & span {
+    position: relative;
+    z-index: 1;
+  }
+
+  &::before {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100px;
+    height: 100px;
+    content: '';
+    background: $main-color;
+    border-radius: 50%;
+    transition: all 0.5s ease-in-out;
+    transform: translateX(-80%) translateY(-25px);
+  }
+
+  &:hover::before {
+    width: 400px;
+    height: 400px;
+    transform: translateX(-1%) translateY(-175px);
+  }
+}
+</style>
---
+++ components/Section.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  width: 90%;
+  padding: 16rem 0;
+  margin: 0 auto;
+}
+</style>
