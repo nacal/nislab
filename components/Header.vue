092634e9b143f639e53d83fe6e0815d1bdf52a38
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-09 03:28:04 +0900

    Develop scrolled header layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header">
+  <header class="header" :class="{ scrolled: scrolledLayout }">
     <h1 class="header__title">
       <nuxt-link to="/" aria-label="トップページに遷移する"
         ><img src="~/assets/images/nislab-wide.png" alt="NISLAB"
@@ -10,7 +10,28 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      scrolledLayout: false,
+      scroll: 0,
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollWindow)
+  },
+  methods: {
+    scrollWindow() {
+      const top = window.innerHeight
+      this.scroll = window.scrollY
+      if (top <= this.scroll) {
+        this.scrolledLayout = true
+      } else {
+        this.scrolledLayout = false
+      }
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
@@ -26,9 +47,15 @@ export default {}
   background-color: #fff;
   filter: drop-shadow(3px 3px 6px rgba(0, 0, 0, 0.3));
   border-radius: 24px;
+  transition: all 0.3s ease-in-out;
 
   &__title {
     width: 180px;
+  }
+
+  &.scrolled {
+    top: 16px;
+    height: 80px;
   }
 }
 </style>
