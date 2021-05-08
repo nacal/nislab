d070c927a34dbd1c966bfa79deae9ab4b540e1fa
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-09 03:44:13 +0900

    Develop footer

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: 360px;
+  background-color: $main-color;
+}
+</style>
