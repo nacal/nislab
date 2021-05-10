b3a16d5d87b7f9c8c4c417789f7ccfb3beeab003
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-05-10 14:18:24 +0900

    Develop topics page

--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -1,5 +1,14 @@
 <template>
   <Section>
     <Title :title="`Topics`" />
+    <Cards class="topics__cards" />
   </Section>
 </template>
+
+<style lang="scss" scoped>
+.topics {
+  &__cards {
+    margin-top: 8rem;
+  }
+}
+</style>
