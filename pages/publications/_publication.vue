98f14aaac1f923e2f92c1510f4077bef57e4320e
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-06-30 20:49:33 +0900

    Develop publication page

---
+++ pages/publications/_publication.vue
@@ -0,0 +1,69 @@
+<template>
+  <Section class="publications">
+    <Title :title="`Publications`" :sub-title="`研究業績`" />
+    <!-- eslint-disable vue/no-v-html -->
+    <article class="publications__content">
+      <h3 class="publications__title">{{ publication.fields.title }}</h3>
+      <div v-html="$md.render(publication.fields.body)" />
+    </article>
+  </Section>
+</template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
+export default {
+  async asyncData({ params }) {
+    return await client
+      .getEntries({
+        content_type: 'publications',
+        'fields.slug': params.publication,
+      })
+      .then((res) => {
+        return { publication: res.items[0] }
+      })
+      .catch()
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.publications {
+  margin: 1rem auto;
+
+  &__title {
+    margin-bottom: 2.5rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  &__content {
+    width: 960px;
+    max-width: 90%;
+    margin: 4rem auto 8rem;
+  }
+}
+</style>
+
+<style lang="scss">
+.publications__content {
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  h3:not(:first-child) {
+    margin-top: 2.5rem;
+  }
+
+  li {
+    padding-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  li + li {
+    border-top: 1px solid #000;
+  }
+}
+</style>
