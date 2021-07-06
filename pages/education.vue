778f1485f08c8f6cfcad86544014800c73b7dbf3
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-07-06 18:54:53 +0900

    Develop education page

--- pages/education.vue
+++ pages/education.vue
@@ -1,5 +1,43 @@
 <template>
   <Section>
     <Title :title="`Education`" :sub-title="`NISLABについて`" />
+    <!-- eslint-disable vue/no-v-html -->
+    <article class="contact__content content">
+      <div v-html="$md.render(body)" />
+    </article>
   </Section>
 </template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
+export default {
+  async asyncData() {
+    return await client
+      .getEntries({
+        content_type: 'pages',
+        'fields.slug': 'education',
+        limit: 1,
+      })
+      .then((res) => {
+        return {
+          body: res.items[0].fields.body,
+        }
+      })
+      .catch()
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  margin: 1rem auto;
+
+  &__content {
+    width: $content-width;
+    max-width: 90%;
+    margin: 4rem auto 8rem;
+  }
+}
+</style>
