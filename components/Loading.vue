8042be1c988812a6b31efb919dce316c050b9998
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-07-20 17:17:36 +0900

    Develop loading

---
+++ components/Loading.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <transition name="pageLoading">
+      <div v-show="pageLoading" class="pageLoading">
+        <img
+          src="../static/apple-touch-icon.png"
+          alt="NISLAB"
+          class="pageLoading__img"
+        />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pageLoading: true,
+    }
+  },
+  mounted() {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        this.pageLoading = false
+      }, 1500)
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pageLoading-leave-active {
+  transition: all 0.5s ease-out;
+}
+.pageLoading-enter,
+.pageLoading-leave-to {
+  opacity: 0;
+}
+
+.pageLoading {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+
+  &__img {
+    max-height: 128px;
+    animation: rotate-anime 3s linear infinite;
+  }
+}
+
+@keyframes rotate-anime {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
